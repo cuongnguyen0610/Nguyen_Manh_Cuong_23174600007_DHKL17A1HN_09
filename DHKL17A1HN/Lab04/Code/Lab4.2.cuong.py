1301289720ff52f33ea93df1a4ca5ad2ca175f28
cuongnguyen0610 <nmcuong.dhkl17a1hn@sv.uneti.edu.vn>
2024-12-30 22:32:02 +0700

    reupdate

--- DHKL17A1HN/Lab04/Code/Lab4.2.cuong.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-path = 'C:/Users/Admin/Documents/lesson1 Data science/Nguyen_Manh_Cuong_23174600007_DHKL17A1HN/DHKL17A1HN/Lab04/Data/a.csv'
-realeas = pd.read_csv(path)
-print(realeas)
